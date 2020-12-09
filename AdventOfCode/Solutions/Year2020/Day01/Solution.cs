818b109e43d8b1dab1ea4f0cbb8a28234d8c6d0c
Sean McCarthy <maxrewind99@gmail.com>
2020-12-09 20:34:21 +1100

    Day 1 - both parts solved using terrible LINQ

--- AdventOfCode/Solutions/Year2020/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day01/Solution.cs
@@ -1,34 +1,81 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2020
 {
 
     class Day01 : ASolution
     {
-
+        
         public Day01() : base(01, 2020, "Report Repair")
         {
-
+            //DebugInput = "1721\n" +
+            //              "979\n" +
+            //              "366\n" +
+            //              "299\n" +
+            //              "675\n" +
+            //             "1456\n";
         }
 
         protected override string SolvePartOne()
         {
-            foreach (var A in Input.ToIntArray("\n"))
-            {
-                foreach (var B in Input.ToIntArray("\n"))
-                {
-                    if (A + B == 2020)
-                        return (A * B).ToString();
-                }
-            }
-            return null;
+            // Trying to use this Advent of Code to get better at Linq
+            // TBH I'm pretty sure the for-loops would've been more efficient than this mess
+
+            int[] input = Input.ToIntArray("\n");
+            var sumQuery =
+                from A in input
+                from B in input
+                where A + B == 2020
+                select A * B;
+            return sumQuery.Any() ? sumQuery.First().ToString() : null;
+            //foreach (var A in Input.ToIntArray("\n"))
+            //{
+            //    foreach (var B in Input.ToIntArray("\n"))
+            //    {
+            //        if (A + B == 2020)
+            //            return (A * B).ToString();
+            //    }
+            //}
+
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            int[] input = Input.ToIntArray("\n");
+            var sumQuery =
+                from A in input
+                from B in input
+                from C in input
+                where A + B + C == 2020
+                select A * B * C;
+            return sumQuery.Any() ? sumQuery.First().ToString() : null;
+            //SearchForSum3(Input.ToIntArray(), 2020)
         }
+
+        // giving up on functions
+
+        // ok I'll be honest I don't actually know how to search for a variable amount of number
+        //int[] SearchForSum2(int[] input, int sum)
+        //{
+        //    var sumQuery =
+        //        from A in input
+        //        from B in input
+        //        where A + B == sum
+        //        select A;
+
+        //    return sumQuery.Distinct().ToArray();
+        //}
+
+        //int[] SearchForSum3(int[] input, int sum)
+        //{
+        //    var sumQuery =
+        //        from A in input
+        //        from B in input
+        //        from C in input
+        //        where A + B + C == sum
+        //        select A;
+
+        //    return sumQuery.Distinct().ToArray();
+        //}
     }
 }
