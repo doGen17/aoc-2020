df3f77ec4246934e9a6fbfc688f018f64aff318c
Sean McCarthy <maxrewind99@gmail.com>
2020-12-09 19:43:59 +1100

    Day 1: Report Repair part 1 solved

--- AdventOfCode/Solutions/Year2020/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day01/Solution.cs
@@ -8,13 +8,21 @@ namespace AdventOfCode.Solutions.Year2020
     class Day01 : ASolution
     {
 
-        public Day01() : base(01, 2020, "")
+        public Day01() : base(01, 2020, "Report Repair")
         {
 
         }
 
         protected override string SolvePartOne()
         {
+            foreach (var A in Input.ToIntArray("\n"))
+            {
+                foreach (var B in Input.ToIntArray("\n"))
+                {
+                    if (A + B == 2020)
+                        return (A * B).ToString();
+                }
+            }
             return null;
         }
 
