e2ffd486718d97914765b6c5a80fa6e8482ef66d
Sean McCarthy <maxrewind99@gmail.com>
2021-03-29 18:24:42 +1100

    Day 06-2: complete

--- AdventOfCode/Solutions/Year2020/Day06/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day06/Solution.cs
@@ -10,7 +10,7 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day06 : ASolution
     {
-
+        string[] groups;
         public Day06() : base(06, 2020, "")
         {
             //DebugInput = "abc\n" +
@@ -28,12 +28,15 @@ namespace AdventOfCode.Solutions.Year2020
             //             "a  \n" +
             //             "   \n" +
             //             "b";
+            // split by blank lines
+            groups = Input.SplitByEmptyLine();
         }
 
+        // identify the questions to which anyone answered "yes"
         protected override string SolvePartOne()
         {
             // split by blank lines
-            var groups = Input.SplitByEmptyLine();
+            //var groups = Input.SplitByEmptyLine();
 
             int sum = 0;
             foreach (var group in groups)
@@ -43,12 +46,39 @@ namespace AdventOfCode.Solutions.Year2020
                 // add the answers to the total sum
                 sum += answers.Count();
             }
-            return $"the sum of all answer counts is [{sum}] { (DebugInput != null ? "(DEBUG)\n(Should be 11)" : string.Empty) }";
+            return string.Format("there were [{0}]{1} answers that anyone in the group answered \"yes\" to.",
+                sum,
+                DebugInput != null ?
+                    "(DEBUG: should be 11)" : string.Empty
+            );
         }
 
+        // identify the questions to which everyone answered "yes"!
         protected override string SolvePartTwo()
         {
-            return null;
+            int sum = 0;
+            foreach (var group in groups)
+            {
+                var people = group.SplitByNewline();
+                char[] answers;
+                answers = people.First().ToCharArray();
+
+                // only contains chars when all people in group answered with them
+                foreach (var p in people)
+                {
+                    answers = answers.Intersect(p).ToArray();
+                }
+
+                // add the answers to the total sum
+                // DEBUG: sum should be 6
+                sum += answers.Count();
+            }
+
+            return string.Format("there were [{0}]{1} answers that anyone in the group answered \"yes\" to.",
+                sum,
+                DebugInput != null ?
+                    "(DEBUG: should be 6)" : string.Empty
+            );
         }
     }
 }
